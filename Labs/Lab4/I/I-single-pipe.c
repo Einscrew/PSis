dd478f3e99f6297b6b751bd992d3bba4441357d8
Einscrew <manel.rodrigo.reis@gmail.com>
2018-03-24 15:47:27 +0000

    Single pipe implementation - Missing response pipe [which is a problem]

--- Labs/Lab4/I/I-single-pipe.c
+++ Labs/Lab4/I/I-single-pipe.c
@@ -23,6 +23,26 @@ int isPrime(long int p){
   return 1;
 }
 
+int pipe_close(int * p, int n, const char name[]){
+    int ret;
+    char c = (n == 0)? '0' : (n == 1)? '1' : 'X';
+
+    switch (n) {
+        case 0:
+            ret = close(p[n]);
+            break;
+        case 1:
+            ret = close(p[n]);
+            break;
+        default:
+            ret = close(p[0]) || close(p[1]);
+            break;
+    }
+    if( ret == -1 ){
+        printf("ERROR closing %s[%c]\t-%s\n", name, c, strerror(errno));
+    }
+    return ret;
+}
 
 int main(int argc, char const *argv[]) {
   int pid, i;
@@ -30,7 +50,6 @@ int main(int argc, char const *argv[]) {
   int setSize = atoi(argv[2]) -1 ;
   long int r = -1;
   int msPipe[2];
-  int smPipe[2];
 
   if(argv[1] == NULL || argv[2] == NULL){
     printf("Usage %s <#slaves> <lenght of set>\n", argv[0]);
@@ -48,62 +67,37 @@ int main(int argc, char const *argv[]) {
   for( i=0; i < atoi(argv[1]); i++){
     if( (pid = fork()) == 0){
       long int v;
-      //int ret = 0;
+      int ret = 0;
 
-      if( close(msPipe[1]) == -1 ){
-          printf("ERROR closing pipe\n");
-          printf("ERROR@%d\t-%s\n",i, strerror(errno));
-          return -1;
-      }
+      ret = ret || pipe_close(msPipe, 1, "@msPipe");
 
       while ( read(msPipe[0], &v, sizeof(v)  ) > 0 ) {
-          printf("receiving %ld\n", v);
+          //printf("receiving %ld\n", v);
         if(isPrime(v) == 1){
           printf("[SON %d] %ld\n", (int)i, v);
         }
       }
-      if(close(msPipe[0]) == -1){
-          printf("ERROR closing msPipe[0] ERROR@%d\t-%s\n",i, strerror(errno));
-          return EXIT_FAILURE;
-      }
-
-      /*if(ret == -1){
-        printf("ERROR@%d\t-%s\n",i, strerror(errno));
-        return
-      }else if( ret == 0){
-        printf( "Nothing more to read\n" );
-
-      }*/
-     return EXIT_SUCCESS;
+      return ret || pipe_close(msPipe, 0, "@msPipe");
 
     }else if( pid < 0){
       printf("Error crating slave %d\n", i);
-      if(close(msPipe[0]) == -1 || close(msPipe[1]) == -1){
-          printf("ERROR closing pipe\n");
-          printf("parent [0] ERROR@%d\t-%s\n",i, strerror(errno));
-          return -1;
-      }
+      pipe_close(msPipe, 2, "@msPipe");
     }
   }
-  if(close(msPipe[0]) == -1){
-      printf("ERROR closing pipe\n");
-      printf("parent [0] ERROR@%d\t-%s\n",i, strerror(errno));
-      return -1;
-  }
+
+  pipe_close(msPipe, 0, "msPipe");
 
   /* Lê todos os números enviados */
   while (setSize > 0) {
     memset(&r, 0, 1);
     r = (long int) rand() % 100000;
 
-    printf("sending %ld\n", r);
+    //printf("sending %ld\n", r);
     write(msPipe[1],&r, sizeof(r));
     setSize--;
   }
-  if( close(msPipe[1]) == -1 ){
-      printf("ERROR closing pipe\n");
-      return -1;
-  }
+
+  pipe_close(msPipe, 1, "msPipe");
 
   while ( wait(&stat) != -1 ) { }
 
